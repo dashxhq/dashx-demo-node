1830270c3dc89371401ebaa23566526ba1b41a43
Selbin C R <selbinzr@gmail.com>
2022-07-04 11:31:48 +0530

    Update readme (#9)
    * Updated readme
    
    * add dashx target environment

--- src/services/dashx.service.js
+++ src/services/dashx.service.js
@@ -4,7 +4,8 @@ const DashX = require('@dashx/node')
 const dx = DashX.default.createClient({
   baseUri: process.env.DASHX_URI,
   publicKey: process.env.DASHX_PUBLIC_KEY,
-  privateKey: process.env.DASHX_PRIVATE_KEY
+  privateKey: process.env.DASHX_PRIVATE_KEY,
+  targetEnvironment: process.env.DASHX_TARGET_ENVIRONMENT
 })
 
 module.exports = dx
