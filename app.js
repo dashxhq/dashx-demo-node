2f161d1eebd40c89c312d706298c989643e70aea
selbin <selbinzr@gmail.com>
2022-07-18 18:18:30 +0530

    split users, posts and products routes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const passport = require('passport')
 
 const userRoutes = require('./src/routes/user.routes')
+const postRoutes = require('./src/routes/post.routes')
+const productRoutes = require('./src/routes/product.routes')
 
 const app = express()
 
@@ -12,7 +14,7 @@ app.use(express.json())
 app.use(passport.initialize())
 require('./src/middlewares/passport.middleware')(passport)
 
-app.use('/', userRoutes)
+app.use('/', userRoutes, postRoutes, productRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`)
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,74 +1,36 @@
 const express = require('express')
 const passport = require('passport')
 
-const usersController = require('../controllers/user.controller')
-const postsController = require('../controllers/post.controller')
-const productsController = require('../controllers/product.controller')
+const userController = require('../controllers/user.controller')
 
 const router = express.Router()
 
 //routes
-router.post('/register', usersController.registerUser)
+router.post('/register', userController.registerUser)
 router.post(
   '/login',
   passport.authenticate('local', {
     session: false,
     failureRedirect: '/unauthorized'
   }),
-  usersController.login
+  userController.login
 )
 
 router.get(
   '/profile',
   passport.authenticate('jwt', { session: false }),
-  usersController.getProfile
+  userController.getProfile
 )
 
 router.patch(
-  '/upload-avatar',
+  '/update-profile',
   passport.authenticate('jwt', { session: false }),
-  usersController.updateProfile
+  userController.updateProfile
 )
 
-router.get('/unauthorized', usersController.unauthorizedLogin)
-router.post('/forgot-password', usersController.forgotPassword)
-router.post('/reset-password', usersController.resetPassword)
-router.post('/contact', usersController.contact)
-
-router.get(
-  '/posts',
-  passport.authenticate('jwt', { session: false }),
-  postsController.getPosts
-)
-
-router.post(
-  '/posts',
-  passport.authenticate('jwt', { session: false }),
-  postsController.createPost
-)
-
-router.put(
-  '/posts/:post_id/toggle_bookmark',
-  passport.authenticate('jwt', { session: false }),
-  postsController.toggleBookmark
-)
-
-router.get(
-  '/posts/bookmarked',
-  passport.authenticate('jwt', { session: false }),
-  postsController.getBookmarkedPosts
-)
-
-router.get(
-  '/products',
-  passport.authenticate('jwt', { session: false }),
-  productsController.getProducts
-)
-
-router.get(
-  '/products/:slug',
-  passport.authenticate('jwt', { session: false }),
-  productsController.getProduct
-)
+router.get('/unauthorized', userController.unauthorizedLogin)
+router.post('/forgot-password', userController.forgotPassword)
+router.post('/reset-password', userController.resetPassword)
+router.post('/contact', userController.contact)
 
 module.exports = router
