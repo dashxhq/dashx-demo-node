578cf84aea225a5dcdb4ac22ac37a3a300c7ab10
Selbin C R <selbinzr@gmail.com>
2022-07-19 16:24:24 +0530

    Update env file (#15)
    * Update env file
    
    * add env.example file
    
    * updated .env.example file

--- src/services/dashx.service.js
+++ src/services/dashx.service.js
@@ -2,7 +2,7 @@ const DashX = require('@dashx/node')
 
 // Initialize DashX SDK
 const dx = DashX.default.createClient({
-  baseUri: process.env.DASHX_URI,
+  baseUri: process.env.DASHX_BASE_URI,
   publicKey: process.env.DASHX_PUBLIC_KEY,
   privateKey: process.env.DASHX_PRIVATE_KEY,
   targetEnvironment: process.env.DASHX_TARGET_ENVIRONMENT
