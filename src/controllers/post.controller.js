bf61a06b153b940a8f17fe6c251db2ee417fadbf
Ajay Kumar <ajay@keepworks.com>
2023-10-27 17:20:15 +0530

    Fixed

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -10,7 +10,7 @@ const createPost = async (req, res) => {
     const {
       rows: [post]
     } = await executeQuery(
-      'INSERT INTO posts (user_id, text, image, audio, video) VALUES ($1, $2, $3, $4) RETURNING *',
+      'INSERT INTO posts (user_id, text, image, audio, video) VALUES ($1, $2, $3, $4, $5) RETURNING *',
       [req.user.id, req.body.text, req.body.image, req.body.audio, req.body.video]
     )
 
